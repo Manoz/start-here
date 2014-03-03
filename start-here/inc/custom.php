e1f555919b27c4fc6c5cf16fdf0f31af0d6c3254
Manoz <manoz@outlook.com>
2014-03-03 02:54:21 +0100

    Add custom.php

---
+++ start-here/inc/custom.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Your custom functions
+ *
+ * Do not add your custom functions in functions.php.
+ * Add them in this file instead :)
+ *
+ * Also, be careful with your functions names.
+ * Don't use 'sh_your_function()' to avoid conflicts \o/
+ *
+ * @package Start Here
+ * @since 1.0.0
+ */
+
+function your_function_name() {
+    // Do something
+}
+// Your hook here if needed
